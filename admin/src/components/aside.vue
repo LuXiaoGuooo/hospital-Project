b6016ee8dc55496ec3ac63e17d9f448b6f87b7a0
陆小果 <luxiaoguo@foxmail.com>
2025-12-07 13:12:18 +0800

    配置B端到导航栏录音

---
+++ admin/src/components/aside.vue
@@ -0,0 +1,35 @@
+<template>
+      <el-menu
+       active-text-color="#ffd04b"
+        background-color="#545c64"
+        class="aside-container"
+        default-active="2"
+        text-color="#fff"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <p class="logo-lg">陪诊服务后台</p>
+        <!-- 菜单组件 -->
+        <tree-menu></tree-menu>
+      </el-menu>
+</template>
+
+<script setup>
+import TreeMenu from './TreeMenu.vue'
+const handleOpen = () => {}
+const handleClose = () => {}
+</script>
+
+<style lang="less" scoped>
+.aside-container{
+    height: 100%;
+    .logo-lg{
+        width: 100%;
+        text-align: center;
+        font-size: 22px;
+        line-height: 60px;
+        height: 50px;
+        color: white;
+    }
+}
+</style>
